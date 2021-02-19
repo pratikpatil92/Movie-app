b92c001e8fe87912fd9e1090b20e946ff9fd4d36
PratikPatil <pratikpatil062@gmail.com>
2021-02-19 16:49:30 +0530

    Lazy loading added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-import store from './store'
-import PopularMovies from './Component/scripts/PopularMovies';
-import TopRatedMovie from './Component/scripts/TopRatedMovie';
-import UpComingMovie from './Component/scripts/UpcomingMovies';
-import PopularMovieDetails from './Component/scripts/PopularMovieDetails';
-import SearchResults from './Component/scripts/SearchResults'
-import SearchMovieDetails from './Component/scripts/SearchMovieDetails'
-import { Provider } from 'react-redux';
-import {BrowserRouter as Router, Route, Switch, Redirect, withRouter} from 'react-router-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Suspense } from "react";
+import store from "./store";
+import { Provider } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+  withRouter,
+} from "react-router-dom";
+import Header from "./Component/scripts/Header";
+import { CircularProgress } from "@material-ui/core";
 
-import Header from './Component/scripts/Header'
+// Lazy loading
+const lazyPopularMovies = React.lazy(() =>
+  import("./Component/scripts/PopularMovies")
+);
+const lazyTopRatedMovie = React.lazy(() =>
+  import("./Component/scripts/TopRatedMovie")
+);
+const lazyUpComingMovie = React.lazy(() =>
+  import("./Component/scripts/PopularMovieDetails")
+);
+const lazySearchResults = React.lazy(() =>
+  import("./Component/scripts/SearchResult")
+);
+const lazySearchMovieDetails = React.lazy(() =>
+  import("./Component/scripts/SearchMovieDetails")
+);
+const lazyPopularMovieDetails = React.lazy(() =>
+  import("./Component/scripts/PopularMovieDetails")
+);
+
+
 
 function App() {
   return (
-      <Provider store={store}>
-        
-      <Router>
-      <Header></Header>
-      <div className="App">
-        <Route exact path="/" component={PopularMovies}></Route>
-        <Route exact path="/top-rated-movie" component={TopRatedMovie}></Route>
-        <Route exact path="/up-coming-movie" component={UpComingMovie}></Route>
-        <Route exact path="/popular-movie-detail/:id" component={PopularMovieDetails}></Route>
-        <Route exact path="/search-movie-detail/:id" component={SearchMovieDetails}></Route>
-        <Route exact path="/search-results/:name" component={SearchResults}></Route>
-        </div>
-      </Router>
+    <Provider store={store}>
+      <Suspense fallback={<CircularProgress />}>
+        <Router>
+          <Header></Header>
+          <div className="App">
+            <Route
+              exact 
+              path="/" 
+              component={lazyPopularMovies}>
+            </Route>
+            <Routes
+              exact
+              path="/top-rated-movie"
+              component={lazyTopRatedMovie}
+            ></Routes>
+            <Route
+              exact
+              path="/up-coming-movie"
+              component={lazyUpComingMovie}
+            ></Route>
+            <Route
+              exact
+              path="/popular-movie-detail/:id"
+              component={lazyPopularMovieDetails}
+            ></Route>
+            <Route
+              exact
+              path="/search-movie-detail/:id"
+              component={lazySearchMovieDetails}
+            ></Route>
+            <Route
+              exact
+              path="/search-results/:name"
+              component={lazySearchResults}
+            ></Route>
+          </div>
+        </Router>
+      </Suspense>
     </Provider>
   );
 }
