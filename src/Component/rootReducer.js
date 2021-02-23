e4000f1789e7f0029b0541ff91f613a4071835bc
sanket2701 <sanket.bafna@somaiya.edu>
2021-02-23 12:49:02 +0530

    First COmmit

--- src/Component/rootReducer.js
+++ src/Component/rootReducer.js
@@ -1,18 +1,18 @@
-import {combineReducers} from 'redux';
-import movieReducer from './Redux/movies/MovieReducer';
-import topRatedMovieReducer from './Redux/movies/TopRatedReducer';
-import upcomingMovieReducer from './Redux/movies/UpcommingReducer';
-import PopularMovieDetailsReducer from './Redux/movies/PopularDetailReducer';
-import castReducer from './Redux/movies/CastReducer';
-import searchReducer from './Redux/movies/SearchMovieReducer';
-import searchcastReducer from './Redux/movies/SearchCastReducer'
+import { combineReducers } from "redux";
+import movieReducer from "./Redux/movies/MovieReducer";
+import topRatedMovieReducer from "./Redux/movies/TopRatedReducer";
+import upcomingMovieReducer from "./Redux/movies/UpcommingReducer";
+import PopularMovieDetailsReducer from "./Redux/movies/PopularDetailReducer";
+import castReducer from "./Redux/movies/CastReducer";
+import searchReducer from "./Redux/movies/SearchMovieReducer";
+import searchcastReducer from "./Redux/movies/SearchCastReducer";
 
-export default combineReducers({
-    movies:movieReducer,
-    to_rated_movies:topRatedMovieReducer,
-    up_coming_movie:upcomingMovieReducer,
-    popular_movie_details:PopularMovieDetailsReducer,
-    casts:castReducer,
-    search_results:searchReducer,
-    search_casts:searchcastReducer,
-})
+export const rootReducer = combineReducers({
+  movies: movieReducer,
+  to_rated_movies: topRatedMovieReducer,
+  up_coming_movie: upcomingMovieReducer,
+  popular_movie_details: PopularMovieDetailsReducer,
+  casts: castReducer,
+  search_results: searchReducer,
+  search_casts: searchcastReducer,
+});
--- src/store.js
+++ src/store.js
@@ -1,15 +1,13 @@
-import {createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './Component/rootReducer'
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { rootReducer } from "./Component/rootReducer";
 
 const initialState = {};
 const middleWare = [thunk];
 const store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-) 
+  rootReducer,
+  initialState,
+  compose(applyMiddleware(...middleWare))
+);
 
 export default store;
